b29023b6182a6ff840a6d4ab286aa1512259fadb
winterfellding <winterfall.ding@gmail.com>
2018-04-10 16:00:32 +0800

    2d peak finder

--- 6.006/lec1.py
+++ 6.006/lec1.py
@@ -10,3 +10,23 @@ def find_peak_1d(ary, lo, hi):
 test_ary = [3, 4, 1, 3, 5, 7, 9, 3, 13, 14]
 print(find_peak_1d(test_ary, 0, len(test_ary) - 1))
 
+def find_peak_2d(ary, r, start_col, col):
+    mid = (col + start_col) // 2
+    max_num, max_idx = 0, 0
+    for i in range(r):
+        if max_num < ary[i][mid]:
+            max_num = ary[i][mid]
+            max_idx = i
+    if mid != 0 and ary[max_idx][mid] < ary[max_idx][mid - 1] :
+        return find_peak_2d(ary, r, start_col, mid - 1)
+    elif mid != col and ary[max_idx][mid] < ary[max_idx][mid + 1]:
+        return find_peak_2d(ary, r, mid + 1, col)
+    else:
+        return mid, max_idx
+
+test_ary = [[10, 8, 10, 10],
+            [14, 13, 12, 11],
+            [15, 9, 11, 21],
+            [16, 17, 19, 20]]
+
+print(find_peak_2d(test_ary, len(test_ary), 0, len(test_ary[0]) - 1))
