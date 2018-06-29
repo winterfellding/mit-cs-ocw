b7c337fdae1b1956cea3045f28aef9928cd7df0e
winterfellding <winterfall.ding@gmail.com>
2018-06-29 21:29:25 +0800

    stable counting sort

--- 6.006/lec7.py
+++ 6.006/lec7.py
@@ -10,6 +10,20 @@ def counting_sort(ary):
                 result.append(i)
     return result
 
-
 # print(counting_sort([3, 4, 1, 1, 2, 3, 4, 5]))
    
+def stable_counting_sort(ary):
+    max_num = max(ary)
+    nums = [0] * (max_num + 1)
+    for i in ary:
+        nums[i] += 1
+    pos = [0] * (max_num + 1)
+    for i in range(1, len(pos)):
+        pos[i] = nums[i-1] + pos[i-1]
+    result = [0] * len(ary)
+    for num in ary:
+        result[pos[num]] = num
+        pos[num] += 1
+    return result
+
+# print(stable_counting_sort([3, 4, 1, 1, 2, 3, 4, 5]))
